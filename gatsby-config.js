f312791c3c816c25ebed2062571af80a01a53951
abyshake-anand <abishakeanand+git@gmail.com>
2023-09-06 11:10:08 +0530

    updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+  pathPrefix: "/gatsby.sample",
   plugins: [
     `gatsby-plugin-image`,
     {
